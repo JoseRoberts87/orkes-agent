412344d1a41bd37a4b3dfb4bea8269ab10def178
Jose Roberts <webterpr@gmail.com>
2025-07-26 15:22:39 -0700

    initial commit

--- index.tsx
+++ index.tsx
@@ -23,9 +23,9 @@ import {
 
 async function test() {
   const clientPromise = orkesConductorClient({
-    keyId: "w7ex00612841-6a59-11f0-b0d8-2aadd5744325", // optional
-    keySecret: "PyZCJZDaiwwGtOoGjkIWoEobCKQL06JPZuRkVcaRwUwezVrk", // optional
-    TOKEN: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImtqbVlNWThEV2VOU1lKZmZSSjFXNSJ9.eyJnaXZlbl9uYW1lIjoiSm9zZSIsImZhbWlseV9uYW1lIjoiUm9iZXJ0cyIsIm5pY2tuYW1lIjoid2VidGVycHIiLCJuYW1lIjoiSm9zZSBSb2JlcnRzIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0tndGp0YVpYelB3NUpUSk8zVElLWHBNUTRzeVlCaWNEcmxQVm5xdXFZREl6RUhBT09pPXM5Ni1jIiwidXBkYXRlZF9hdCI6IjIwMjUtMDctMjZUMTg6MzE6MTQuMDU1WiIsImVtYWlsIjoid2VidGVycHJAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOi8vYXV0aC5vcmtlcy5pby8iLCJhdWQiOiJNeUhKWXVUc3FOTDhEYUxJR3dvdTZmU2F4ekYzVEZyVyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA1MTU3MjgyNDM0NDg3MDE0ODcyIiwiaWF0IjoxNzUzNTU0Njc1LCJleHAiOjE3NTM1OTA2NzUsInNpZCI6ImFhQW5WYkpua19heDhHOEo0eXBQQlFKczdZcXdmNWhRIiwibm9uY2UiOiJTVUZ1YjBWMFVFMW1PV0o2VFdsaVIzY3RYMmR1UzNWVFZpNHhOVU5tVDJnMlNISjJNRkpsUVhCWldRPT0ifQ.T66DYv2lYCuwIsi4hVfSxIvCWQO_q_0UQTxiLLaZ8IXCmFZIY5zdg_EBUoBgRNzWNrd-gAbaICUarw606X4hmOmYI0mcS7bCqaH3Jtg-NJ8OwIJn8z2YQYm65a7OOml9N7Ud0fU4hKQXyYdZJ-a2FRy9j53axn5vt1z2cLSRaJbf8pxPoac0Sl7HV-gHFsF86EFmj8fH9g94TZF6evVYgkgoX1cojrP8spw7aZ-r_vnQOr_UL-t6_xo610Mtu7lqQJ8K9VcjA-x0FurYG-5XUUkrdM66P_6LWFDLfVa0gChcoNbpIfnN1kwzSYO_JC6hdhmkQ3YgbNAw2IDlFV-eog",
+    keyId: "", // optional
+    keySecret: "", // optional
+    TOKEN: "",
     serverUrl: "https://developer.orkescloud.com/api"
   });
 
